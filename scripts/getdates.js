dc47ab48015118e81cfb71aa48c9ef02a88b6410
diazeliel <cmadrid@byupathway.edu>
2025-03-18 11:29:55 -0600

    changes temples

--- scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Get the date the document was last modified
-const lastModifiedDate = new Date(document.lastModified).toLocaleDateString('en-us');
-const lastModifiedTime = new Date(document.lastModified).toLocaleTimeString('en-US');
-const lastModifiedParagraph = document.getElementById('lastmodified');
-lastModifiedParagraph.innerHTML = `last modified on: ${lastModifiedDate} ${lastModifiedTime}`;
